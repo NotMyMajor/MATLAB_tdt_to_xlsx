5069f37c57848f9450c8ec7b299b8906195dede4
Rhys Switzer <50533984+NotMyMajor@users.noreply.github.com>
2022-01-25 17:48:27 -0600

    Update tdt_to_xlsx.m

--- tdt_to_xlsx.m
+++ tdt_to_xlsx.m
@@ -1,3 +1,15 @@
+%  Copyright (C) 2022  Rhys Switzer
+
+%    This program is free software: you can redistribute it and/or modify
+%    it under the terms of the GNU General Public License as published by
+%    the Free Software Foundation, either version 3 of the License, or
+%    (at your option) any later version.
+
+%    This program is distributed in the hope that it will be useful,
+%    but WITHOUT ANY WARRANTY; without even the implied warranty of
+%    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+%    GNU General Public License for more details.
+
 function output_file = tdt_to_xlsx(tdt_file, options)
 
 % Specify input arguments, name-value pairs, and default values.
