f65a538807c446c47a9f1636bff10d757f890366
Rhys Switzer <50533984+NotMyMajor@users.noreply.github.com>
2022-01-25 17:24:54 -0600

    Update tdt_to_xlsx.m

--- tdt_to_xlsx.m
+++ tdt_to_xlsx.m
@@ -1,5 +1,6 @@
 function output_file = tdt_to_xlsx(tdt_file, options)
 
+% Specify input arguments, name-value pairs, and default values.
 arguments
    
     tdt_file (1,1) string {mustBeFile}
@@ -10,6 +11,7 @@ arguments
     
 end
 
+% Convert .tdt file to .txt and save.
 [~, tempfile] = fileparts(tdt_file);
 options.XLSXFileName = append(tempfile, ".xlsx");
 con_file = fullfile(options.DestinationPath, tempfile);
@@ -19,14 +21,22 @@ if not(exist(con_file, 'file'))
 else
     options.SaveTXT = 1;
 end
+
+% Read in cell array from .txt file.
 cur_tdt = readcell(con_file, 'Delimiter', '\t');
+
+% Get rid of any <missing> values to prepare for saving.
 mask = cellfun(@(x) ismissing(x), cur_tdt, 'UniformOutput', false);
 mask2 = cell2mat(cellfun(@(x) length(x)>1, cur_tdt, 'UniformOutput', false));
 mask(mask2) = {false};
 mask = cell2mat(mask);
 cur_tdt(mask) = {NaN};
+
+% Export cell array as .xlsx file to destination folder.
 output_file = fullfile(options.DestinationPath, options.XLSXFileName);
 writecell(cur_tdt, output_file, 'WriteMode', options.WriteMode);
+
+% Delete the .txt file if indicated.
 if not(options.SaveTXT)
     delete(con_file);
 end
